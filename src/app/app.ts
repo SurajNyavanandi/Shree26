d8107502986a9e988a552c928ff86a96497c787a
SurajNyavanandi <surajdec11@gmail.com>
2025-08-16 19:19:45 +0530

    added components header,body & footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Body } from './components/body/body';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Header,Body,Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
